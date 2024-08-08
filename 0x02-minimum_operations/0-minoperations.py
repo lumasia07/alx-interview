6e6ccdedc6bcdfc78ebf9a73f337b572cbc8ec29
Lumasia <lumasiastancelaus@gmail.com>
2024-08-08 19:12:16 +0300

    pycodestyle fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,14 +6,14 @@ def minOperations(n):
     """Return number of minimum operations"""
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
+
     return operations
