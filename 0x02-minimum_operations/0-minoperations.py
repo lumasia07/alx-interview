09957a1e745be2a2fc372992098108dcec147972
Lumasia <lumasiastancelaus@gmail.com>
2024-08-08 19:10:01 +0300

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Minimum operations"""
+
+
+def minOperations(n):
+    """Return number of minimum operations"""
+    if n <= 1:
+        return 0
+    
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+    
+    return operations
