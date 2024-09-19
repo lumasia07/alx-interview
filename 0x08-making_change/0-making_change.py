0c91cba315ea8d2feac9934ac480a92b80942791
Lumasia <lumasiastancelaus@gmail.com>
2024-09-19 10:30:04 +0300

    Improved efficiency using Greedy algos

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,11 +6,17 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    coins.sort(reverse=True)
+
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
     for coin in coins:
         for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+            if dp[i - coin] != float('inf'):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        if dp[total] != float('inf'):
+            break
 
     return dp[total] if dp[total] != float('inf') else -1
