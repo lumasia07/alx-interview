bd0394cba65a7f106906a299e4b6a1eabd7e0d03
Lumasia <lumasiastancelaus@gmail.com>
2024-10-03 16:18:00 +0300

    Prime Game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Prime numbers game
+"""
+
+
+def isWinner(x, nums):
+    """Find the winner of the game
+    """
+    if x <= 0 or not nums:
+        return None
+
+    max_n = max(nums)
+
+    is_prime = [True] * (max_n + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(max_n ** 0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_n + 1, i):
+                is_prime[j] = False
+
+    prime_count = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if is_prime[i] else 0)
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if prime_count[n] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
